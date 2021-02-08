8b39eada4542a97f6090249be6c3bc5d3565f218
vikasz <vikas.zingade@skillate.com>
2021-02-08 11:55:40 +0530

    Read from env file with environs

--- pika_pika/settings/settings.py
+++ pika_pika/settings/settings.py
@@ -1,9 +1,10 @@
 from environs import Env
 
 env = Env()
+env.read_env()
 
 RABBITMQ_HOST: str = env.str("RABBITMQ_HOST")
 RABBITMQ_PASSWORD: str = env.str("RABBITMQ_PASSWORD")
-RABBITMQ_PORT: str = env.str("RABBITMQ_PORT")
+RABBITMQ_PORT: int = env.int("RABBITMQ_PORT")
 RABBITMQ_USERNAME: str = env.str("RABBITMQ_USERNAME")
 RABBITMQ_VIRTUAL_HOST: str = env.str("RABBITMQ_VIRTUAL_HOST")
